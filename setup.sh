671da8fc4a49523ab62d09b5168ee6716e13b26d
Alexander Yeagle <AlexanderYeagle@MacBook-Pro.local>
2023-11-18 11:28:55 -0800

    hehe

--- setup.sh
+++ setup.sh
@@ -1,14 +1,33 @@
+# chmod +x gitgo.sh
+
+# # Get the absolute path to the script's directory
+# SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# echo "Abs path recorded: $SCRIPT_DIR"
+
+
+# nano ~/.zshrc
+
+# # Append the export PATH line to your script's file
+# echo 'export PATH="$PATH:'"$SCRIPT_DIR"'"' >> "$SCRIPT_DIR/gitgo.sh"
+
+# source ~/.zshrc  # For Bash
+
+
+#!/bin/bash
+
+# Make the script executable
 chmod +x gitgo.sh
 
-# Get the absolute path to the script's directory
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# Get the absolute path to the script's directory using realpath (macOS and Linux compatible)
+SCRIPT_DIR=$(realpath "$(dirname "$0")")
 
 echo "Abs path recorded: $SCRIPT_DIR"
 
+# Append the export PATH line to ~/.zshrc
+echo 'export PATH="$PATH:'"$SCRIPT_DIR"'"' >> ~/.zshrc
 
-nano ~/.zshrc
+# Source ~/.zshrc to apply changes to the current shell session
+source ~/.zshrc
 
-# Append the export PATH line to your script's file
-echo 'export PATH="$PATH:'"$SCRIPT_DIR"'"' >> "$SCRIPT_DIR/gitgo.sh"
-
-source ~/.zshrc  # For Bash
+echo "Added $SCRIPT_DIR to your PATH in ~/.zshrc"
