10f66dc84cc7a353d022c81bc491e7cee5569d3b
Alexander Yeagle <AlexanderYeagle@MacBook-Pro.local>
2023-11-18 11:54:09 -0800

    making changes

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@
 
 # Make the script executable
 chmod +x gitgo.sh
+chmod +x checkurl.sh
 
 # Get the absolute path to the script's directory using realpath (macOS and Linux compatible)
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
@@ -31,14 +32,7 @@ echo 'export PATH="$PATH:'"$SCRIPT_DIR"'"' >> ~/.zshrc
 source ~/.zshrc
 
 echo "Added $SCRIPT_DIR to your PATH in ~/.zshrc"
+touch .gitgo_repo_url
 
-if [ -z "$GITGO_GITHUB_REPO_URL" ]; then
-  # If not set, prompt the user to provide a GitHub repository URL
-  read -p "Enter the GitHub repository URL: " user_url
-  # Save the URL to a file
-  echo "$user_url" > ~/.gitgo_repo_url
-  # Save the URL as an environment variable
-  export GITGO_GITHUB_REPO_URL="$user_url"
-  echo "GitHub repository URL set as: $GITGO_GITHUB_REPO_URL"
-fi
 
+url=$(source checkurl.sh)
