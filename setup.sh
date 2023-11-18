64ad9673f9a18a76e3b215554775aa2ff8582d85
Alexander Yeagle <AlexanderYeagle@MacBook-Pro.local>
2023-11-18 11:35:14 -0800

    hshdhd

--- setup.sh
+++ setup.sh
@@ -31,3 +31,14 @@ echo 'export PATH="$PATH:'"$SCRIPT_DIR"'"' >> ~/.zshrc
 source ~/.zshrc
 
 echo "Added $SCRIPT_DIR to your PATH in ~/.zshrc"
+
+if [ -z "$GITGO_GITHUB_REPO_URL" ]; then
+  # If not set, prompt the user to provide a GitHub repository URL
+  read -p "Enter the GitHub repository URL: " user_url
+  # Save the URL to a file
+  echo "$user_url" > ~/.gitgo_repo_url
+  # Save the URL as an environment variable
+  export GITGO_GITHUB_REPO_URL="$user_url"
+  echo "GitHub repository URL set as: $GITGO_GITHUB_REPO_URL"
+fi
+
